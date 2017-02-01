9881fab39460b4de39bc62082583df279c0e97ff
John Sarris Burke <jsburke@bu.edu>
2017-02-01 15:27:50 -0500

    create func to graph output from combine_2d.c

---
+++ Lab1/matlabUtil/graph_2d.m
@@ -0,0 +1,10 @@
+function graph_2d(file)
+data = csvread(file);
+figure;
+hold on;
+plot1 = plot(data(:,1), data(:,2));
+Leg1 = 'Forward';
+plot2 = plot(data(:,1), data(:,3));
+Leg2 = 'Reverse';
+legend([plot1; plot2], [Leg1; Leg2]);
+end
