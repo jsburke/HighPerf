645e14bdcace41763673dd266ee6f1a1c02b6d47
John Sarris Burke <jsburke@bu.edu>
2017-02-02 14:13:54 -0500

    update for fixed code, produce more files

--- Lab1/scripts/combineBuild.sh
+++ Lab1/scripts/combineBuild.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 gcc -O1 -lrt combine2d.c -o combine2d
-./combine2d
+#       DELTA ITERS
+./combine2d 2 10
+./combine2d 2 20
+./combine2d 2 40
+./combine2d 2 80
+./combine2d 2 160
+./combine2d 2 320
+./combine2d 8 10
+./combine2d 8 20
+./combine2d 8 40
+./combine2d 8 80
+./combine2d 8 160
+./combine2d 8 320
+./combine2d 16 10
+./combine2d 16 20
+./combine2d 16 40
+./combine2d 16 80
+./combine2d 16 160
+./combine2d 16 320
+./combine2d 64 10
+./combine2d 64 20
+./combine2d 64 40
+./combine2d 64 80
+./combine2d 64 160
+./combine2d 64 320
 mv *.csv ./p1_data/
