a4e7387f354a8fe3cfeda3d8ac531a060dc1d4c2
John Sarris Burke <jsburke@bu.edu>
2017-02-02 14:08:33 -0500

    update for log base 10 plotting

--- Lab1/matlabUtil/graph_2d.m
+++ Lab1/matlabUtil/graph_2d.m
@@ -1,11 +1,10 @@
 function graph_2d(file)
-% Should alter to rename axes
 data = csvread(file);
 figure;
 hold on;
-plot1 = plot(data(:,1), data(:,2));
+plot1 = plot(data(:,1), log10(data(:,2)));
 Leg1 = 'Forward';
-plot2 = plot(data(:,1), data(:,3));
+plot2 = plot(data(:,1), log10(data(:,3)));
 Leg2 = 'Reverse';
 legend([plot1; plot2], [Leg1; Leg2]);
 end
