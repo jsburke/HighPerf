a3963f511018f979abc465c41faca11178df8ef2
John Sarris Burke <jsburke@bu.edu>
2017-02-02 08:17:27 -0500

    create loops on ITERS & BASE, dump to file, alter CPG

--- Lab1/code/combine2d.c
+++ Lab1/code/combine2d.c
@@ -8,17 +8,26 @@
 #include <math.h>
 
 #define GIG 1000000000
-#define CPG 2.0           // Cycles per GHz -- Adjust to your computer
+#define CPG 2.9           // changed to 2.9 for lab computers
 
-#define BASE  0
-#define ITERS 30
-#define DELTA 200
+
+// the following have been changed to int so that we can loop on them
+// to generate multiple files per run without the need to recompile
+int BASE = 0;
+int ITERS = 5;
+int DELTA = 2;
+#define ITERSMAX 25
+#define DELTAMAX 8
+#define ITERSJUMP 5
+#define DELTAJUMP 2
 
 #define OPTIONS 2
 #define IDENT 1
 #define OP *
 
-typedef double data_t;
+#define FILE_PREFIX ((const unsigned char*) "intMul_")
+
+typedef int data_t;
 
 /* Create abstract data type for vector */
 typedef struct {
@@ -42,45 +51,61 @@ main(int argc, char *argv[])
   void combine2D(vec_ptr v, data_t *dest);
   void combine2D_rev(vec_ptr v, data_t *dest);
 
-  long int i, j, k;
+  long int d, i, j, k;
   long int time_sec, time_ns;
-  long int MAXSIZE = BASE+(ITERS+1)*DELTA;
+  long int MAXSIZE;
 
-  //printf("\n Hello World -- 2D Combine \n");
+  char filename [255] = {0};
+  FILE *fp;
 
-  // declare and initialize the vector structure
-  vec_ptr v0 = new_vec(MAXSIZE);
-  data_holder = (data_t *) malloc(sizeof(data_t));
-  init_vector(v0, MAXSIZE);
+  for(d = DELTA; d <= DELTAMAX; d *= DELTAJUMP)
+  {
+    for(i = ITERS; i <= ITERSMAX; i *= ITERSJUMP)
+    {
+      MAXSIZE = BASE+(i+1)*d;
 
-  OPTION = 0;
-  for (i = 0; i < ITERS; i++) {
-    set_vec_length(v0,BASE+(i+1)*DELTA);
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
-    combine2D(v0, data_holder);
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    time_stamp[OPTION][i] = diff(time1,time2);
-  }
+        vec_ptr v0 = new_vec(MAXSIZE);
+        data_holder = (data_t *) malloc(sizeof(data_t));
+        init_vector(v0, MAXSIZE);
 
-  OPTION++;
-  for (i = 0; i < ITERS; i++) {
-    set_vec_length(v0,BASE+(i+1)*DELTA);
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
-    combine2D_rev(v0, data_holder);
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    time_stamp[OPTION][i] = diff(time1,time2);
-  }
+        OPTION = 0;  // forward
+          for (k = 0; k < ITERS; k++) 
+          {
+           set_vec_length(v0,BASE+(k+1)*d);
+          clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+          combine2D(v0, data_holder);
+          clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+          time_stamp[OPTION][k] = diff(time1,time2);
+        }
 
-  for (i = 0; i < ITERS; i++) {
-    printf("\n%d, ", BASE+(i+1)*DELTA);
-    for (j = 0; j < OPTIONS; j++) {
-      if (j != 0) printf(", ");
-      printf("%ld", (long int)((double)(CPG)*(double)
-		 (GIG * time_stamp[j][i].tv_sec + time_stamp[j][i].tv_nsec)));
+        OPTION++; // reverse
+        for (k = 0; k < ITERS; k++) 
+        {
+          set_vec_length(v0,BASE+(k+1)*d);
+          clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+          combine2D_rev(v0, data_holder);
+          clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+          time_stamp[OPTION][k] = diff(time1,time2);
+        }
+
+        //print to file
+
+        sprintf(filename, "%sI%d_D%d.csv", FILE_PREFIX, i, d);
+        fp = fopen(filename, "w");
+        for (k = 0; k < ITERS; k++) 
+        {
+          fprintf(fp,"\n%d, ", BASE+(k+1)*d);
+          for (j = 0; j < OPTIONS; j++) 
+          {
+              if (j != 0) fprintf(fp,", ");
+              fprintf(fp,"%ld", (long int)((double)(CPG)*(double) (GIG * time_stamp[j][k].tv_sec + time_stamp[j][k].tv_nsec)));
+            } 
+        }
+        fclose(fp);
     }
-  }
+}
 
-  printf("\n");
+  //printf("\n");
   
 }/* end main */
 /*********************************/
