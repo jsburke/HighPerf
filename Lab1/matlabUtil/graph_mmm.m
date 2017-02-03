29d42d47a667457f0c95cd4a2335719379394363
Burke <jsburke@signals50.bu.edu>
2017-02-03 09:35:41 -0500

    remove log of y axis to look like B&O

--- Lab1/matlabUtil/graph_mmm.m
+++ Lab1/matlabUtil/graph_mmm.m
@@ -2,8 +2,8 @@ function graph_mmm(file)
 data = csvread(file);
 figure;
 hold on;
-plot1 = plot(data(:,1), log10(data(:,2)),'b');
-plot2 = plot(data(:,1), log10(data(:,3)),'r');
-plot3 = plot(data(:,1), log10(data(:,4)),'g');
+plot1 = plot(data(:,1), (data(:,2)),'b');
+plot2 = plot(data(:,1), (data(:,3)),'r');
+plot3 = plot(data(:,1), (data(:,4)),'g');
 legend('ijk','kij','jki','Location','southeast')
 end
