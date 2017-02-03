6e1f77b491ca55502087e9a47903c7eb7cd26dd5
Burke <jsburke@vlsi29.bu.edu>
2017-02-03 08:57:46 -0500

    add script to build & run mmm_inter.c

---
+++ Lab1/scripts/mmmBuild.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+gcc -O1 -lrt mmm_inter.c -o mmm_inter
+#       BASE DELTA ITERS
+./mmm_inter 0 2 2
+./mmm_inter 0 2 4
+./mmm_inter 0 2 6
+./mmm_inter 0 2 8
+./mmm_inter 0 2 10
+./mmm_inter 0 2 12
+
+./mmm_inter 0 4 2
+./mmm_inter 0 4 4
+./mmm_inter 0 4 6
+./mmm_inter 0 4 8
+./mmm_inter 0 4 10
+./mmm_inter 0 4 12
+
+./mmm_inter 0 16 2
+./mmm_inter 0 16 4
+./mmm_inter 0 16 6
+./mmm_inter 0 16 8
+./mmm_inter 0 16 10
+./mmm_inter 0 16 12
+
+./mmm_inter 0 64 2
+./mmm_inter 0 64 4
+./mmm_inter 0 64 6
+./mmm_inter 0 64 8
+./mmm_inter 0 64 10
+./mmm_inter 0 64 12
+mv *.csv ./part2_data/
