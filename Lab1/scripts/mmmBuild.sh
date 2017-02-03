75ecc2858878c9d653d058d5bd8ad2448184354f
Burke <jsburke@signals50.bu.edu>
2017-02-03 09:48:43 -0500

    add newlines, new invocations for more difficult runs

--- Lab1/scripts/mmmBuild.sh
+++ Lab1/scripts/mmmBuild.sh
@@ -5,28 +5,37 @@ gcc -O1 -lrt mmm_inter.c -o mmm_inter
 ./mmm_inter 0 2 4
 ./mmm_inter 0 2 6
 ./mmm_inter 0 2 8
-./mmm_inter 0 2 10
-./mmm_inter 0 2 12
+printf "\n"
 
 ./mmm_inter 0 4 2
 ./mmm_inter 0 4 4
 ./mmm_inter 0 4 6
 ./mmm_inter 0 4 8
-./mmm_inter 0 4 10
-./mmm_inter 0 4 12
+printf "\n"
 
 ./mmm_inter 0 16 2
 ./mmm_inter 0 16 4
 ./mmm_inter 0 16 6
 ./mmm_inter 0 16 8
-./mmm_inter 0 16 10
-./mmm_inter 0 16 12
+printf "\n"
 
 ./mmm_inter 0 64 2
 ./mmm_inter 0 64 4
 ./mmm_inter 0 64 6
 ./mmm_inter 0 64 8
-./mmm_inter 0 64 10
-./mmm_inter 0 64 12
+printf "\n"
+
+./mmm_inter 0 128 2
+./mmm_inter 0 128 4
+./mmm_inter 0 128 6
+./mmm_inter 0 128 8
+printf "\n"
+
+./mmm_inter 0 256 2
+./mmm_inter 0 256 4
+./mmm_inter 0 256 6
+./mmm_inter 0 256 8
+printf "\n"
+
 mv *.csv ./part2_data/
 rm mmm_inter
