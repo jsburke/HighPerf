bebc3d62dc44f9bc9bf3574fe021d083abbc71da
Burke <jsburke@signals50.bu.edu>
2017-02-03 09:19:05 -0500

    add line color specifications

--- Lab1/matlabUtil/graph_2d.m
+++ Lab1/matlabUtil/graph_2d.m
@@ -2,9 +2,9 @@ function graph_2d(file)
 data = csvread(file);
 figure;
 hold on;
-plot1 = plot(data(:,1), log10(data(:,2)));
+plot1 = plot(data(:,1), log10(data(:,2)),'b');
 Leg1 = 'Forward';
-plot2 = plot(data(:,1), log10(data(:,3)));
+plot2 = plot(data(:,1), log10(data(:,3)),'r');
 Leg2 = 'Reverse';
 legend([plot1; plot2], [Leg1; Leg2]);
 end
