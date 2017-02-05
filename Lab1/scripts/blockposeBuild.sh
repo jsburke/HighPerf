ef83347e0b143d863d0a77eadf7f57165564fd71
jsburke <jsburke@bu.edu>
2017-02-04 22:47:41 -0500

    add script to build & test blocking transpose.  Some tests may take a while.

---
+++ Lab1/scripts/blockposeBuild.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "building and running blockpose"
+curdir=${PWD##*/}
+if [ $curdir != "code" ]
+	then
+	echo "please run in code directory"
+	exit
+fi
+
+if [ -d ./part3_data ]
+	then
+	echo "part4_data directory found"
+else
+	echo "making directory for data output files"
+	mkdir part3_data
+fi
+
+echo "compile blockpose"
+gcc -O1 -lrt test_transpose_block.c -o blockpose
+
+# Each of these really deserves reevaluation based on the 
+# data type
+# 8 should be good for normal floats if I'm not too tired atm
+echo "run tests"
+./blockpose 1 128 3
+./blockpose 1 256 3 
+./blockpose 1 512 3 
+./blockpose 1 1024 3 
+printf "\n"
+
+./blockpose 2 128 3
+./blockpose 2 256 3 
+./blockpose 2 512 3 
+./blockpose 2 1024 3 
+printf "\n"
+
+./blockpose 4 128 3
+./blockpose 4 256 3 
+./blockpose 4 512 3 
+./blockpose 4 1024 3 
+printf "\n"
+
+mv *.csv ./part3_data
+rm blockpose
+echo "blockpose complete"
