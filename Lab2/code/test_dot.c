e4f7611a9c8c4682b31b6dcb98fa60abbba4d982
Robert Munafo <mrob27@gmail.com>
2017-02-11 12:14:28 -0500

    More cleanup, output in CPS, add dotprod2()

--- Lab2/code/test_dot.c
+++ Lab2/code/test_dot.c
@@ -1,6 +1,6 @@
 // -*- C++ -*-
 //
-// gcc test_dot.c -lrt -o tdt
+// gcc test_dot.c -lrt -o tdt && ./tdt
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,10 +8,11 @@
 #include <time.h>
 #include <math.h>
 
-#define SIZE 10000000
 #define ITERS 20
+
 #define DELTA 1000
 #define BASE 0
+#define ITER_VECLEN(i) (BASE+((i)+1)*DELTA)
 
 double CPS = 2.9e9;    // Cycles per second -- Will be recomputed at runtime
 
@@ -31,7 +32,6 @@ long int get_vec_length(vec_ptr v);
 vec_ptr new_vec(long int len);
 int set_vec_length(vec_ptr v, long int index);
 int init_vector(vec_ptr v, long int len);
-double dotprod4(vec_ptr a, vec_ptr b);
 data_t *get_vec_start(vec_ptr v);
 struct timespec ts_diff(struct timespec start, struct timespec end);
 double ts_sec(struct timespec ts);
@@ -45,46 +45,78 @@ typedef union {
 #define RDTSC(cpu_c) __asm__ __volatile__ ("rdtsc" : \
                      "=a" ((cpu_c).int32.lo), "=d"((cpu_c).int32.hi))
 
+
+/* Each of variation of the task being benchmarked */
+#define METHODS 3
+double dotprod1(vec_ptr a, vec_ptr b);
+double dotprod2(vec_ptr a, vec_ptr b);
+
+
 /*****************************************************************************/
 int main(int argc, char *argv[])
 {
   struct timespec cal_start, cal_end;
   tsc_counter tsc_start, tsc_end;
   struct timespec time1, time2;
-  struct timespec time_stamp[ITERS+1];
+  struct timespec time_stamp[ITERS+1][METHODS];
+  long int elements[ITERS+1];
   long int i, j, k;
   long long int time_sec, time_ns;
-  long int MAXSIZE = BASE+(ITERS+1)*DELTA;
+  long int maxsize = ITER_VECLEN(ITERS);
 
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cal_start);
   RDTSC(tsc_start);
 
   // declare and initialize the vector structure
-  vec_ptr v0 = new_vec(MAXSIZE);
-  vec_ptr v1 = new_vec(MAXSIZE);
-  init_vector(v0, MAXSIZE);
-  init_vector(v1, MAXSIZE);
+  vec_ptr v0 = new_vec(maxsize);
+  vec_ptr v1 = new_vec(maxsize);
+  init_vector(v0, maxsize);
+  init_vector(v1, maxsize);
 
   for (i = 0; i < ITERS; i++) {
-    set_vec_length(v0, BASE+(i+1)*DELTA);
-    set_vec_length(v1, BASE+(i+1)*DELTA);
+    set_vec_length(v0, ITER_VECLEN(i));
+    set_vec_length(v1, ITER_VECLEN(i));
+    elements[i] = ITER_VECLEN(i);
+
+    /* Measure each different type of optimisation */
+
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
-    dotprod4(v0, v1);
+    dotprod1(v0, v1);
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    time_stamp[i] = ts_diff(time1,time2);
+    time_stamp[i][0] = ts_diff(time1,time2);
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    dotprod2(v0, v1);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+    time_stamp[i][1] = ts_diff(time1,time2);
+
   }
 
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cal_end);
   RDTSC(tsc_end);
 
-  CPS = ((double)(tsc_end.int64-tsc_start.int64))
-                                        / ts_sec(ts_diff(cal_start, cal_end));
-  printf("CPS == %g\n", CPS);
+  {
+    double total_time = ts_sec(ts_diff(cal_start, cal_end));
+    double total_cycles = (double)(tsc_end.int64-tsc_start.int64);
+    CPS = total_cycles / total_time;
+    printf("Computed CPS == %g\n", CPS);
+  }
 
   /* output times */
+  printf("Vector Size, Simple Loop, Unrolled 2x\n");
   for (i = 0; i < ITERS; i++) {
-    printf("%ld,  ", ((long int) (BASE+(i+1)*DELTA)));
-    printf("%ld\n", ((long int) (CPS * ts_sec(time_stamp[i]))));
+    long int cycles;
+    printf("%ld", ((long int) ITER_VECLEN(i)));
+
+    cycles = (long int) (CPS * ts_sec(time_stamp[i][0]));
+    /* printf(", %ld", cycles); */
+    printf(", %f", ((double)cycles) / ((double) elements[i]));
+
+    cycles = (long int) (CPS * ts_sec(time_stamp[i][1]));
+    /* printf(", %ld", cycles); */
+    printf(", %f", ((double)cycles) / ((double) elements[i]));
+
+    printf("\n");
   }
 
   return 0;
@@ -171,7 +203,7 @@ double ts_sec(struct timespec ts)
 /* Dot product, Accumulating result in a local variable
  * (This is analogous to the 'Combine4' version of the vector reduction
  * functions in test_combine1-7.c) */
-double dotprod4(vec_ptr a, vec_ptr b)
+double dotprod1(vec_ptr a, vec_ptr b)
 {
   long int i;
   long int alen = get_vec_length(a);
@@ -182,7 +214,29 @@ double dotprod4(vec_ptr a, vec_ptr b)
   data_t acc = 0;
   if (alen == blen) {
     for (i = 0; i < alen; i++) {
-      acc = acc + adata[i] + bdata[i];
+      acc = acc + (adata[i] * bdata[i]);
+    }
+  }
+  return acc;
+}
+
+/* This version unrolls the loop a bit. */
+double dotprod2(vec_ptr a, vec_ptr b)
+{
+  long int i;
+  long int alen = get_vec_length(a);
+  long int blen = get_vec_length(b);
+  data_t *adata = get_vec_start(a);
+  data_t *bdata = get_vec_start(b);
+
+  data_t acc = 0;
+  if (alen == blen) {
+    for (i = 0; i < alen; i+=2) {
+      acc = acc + (adata[i] * bdata[i]);
+      acc = acc + (adata[i+1] * bdata[i+1]);
+    }
+    if (i < alen) {
+      acc = acc + (adata[i] * bdata[i]);
     }
   }
   return acc;
