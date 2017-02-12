1cf9d2159993ddf3cb81aed1f63164cea79be995
Robert Munafo <mrob27@gmail.com>
2017-02-11 20:06:31 -0500

    Add test_branch.c

--- Lab2/code/test_branch.c
+++ Lab2/code/test_branch.c
@@ -1,6 +1,6 @@
-/*****************************************************************************/
+/* -*- C++ -*- **************************************************************/
 
-// gcc -o test_branch test_branch.c -lrt
+// gcc -O1 test_branch.c -lrt -o tbr
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,13 +11,15 @@
 #define GIG 1000000000
 #define CPG 2.9   
 
-#define ITERS 50
-#define DELTA 10
 #define BASE 0
+#define DELTA 5000
+#define ITERS 20
 
-#define OPTIONS 4
-#define IDENT 1.0
-#define OP *
+#define OPTIONS 10
+
+#define ITER_VECLEN(i) (BASE+((i)+1)*DELTA)
+
+#define FILE_PREFIX ((const unsigned char*) "branch_")
 
 typedef double data_t;
 
@@ -27,107 +29,221 @@ typedef struct {
   data_t *data;
 } vec_rec, *vec_ptr;
 
+/* ---------------------------------------------------------------------------
+| Make the CPU busy, and measure CPS (cycles per second)
+|
+| Copy this code into other programs as desired
+|
+| double ts_sec(ts): converts a timespec into seconds
+| timespec ts_diff(ts1, ts2): computes interval between two timespecs
+| measure_cps()
+--------------------------------------------------------------------------- */
+double CPS = 2.9e9;    // Cycles per second -- Will be recomputed at runtime
+int clock_gettime(clockid_t clk_id, struct timespec *tp);
+
+typedef union {
+  unsigned long long int64;
+  struct {unsigned int lo, hi;} int32;
+} mcps_tctr;
+
+#define MCPS_RDTSC(cpu_c) __asm__ __volatile__ ("rdtsc" : \
+                     "=a" ((cpu_c).int32.lo), "=d"((cpu_c).int32.hi))
+
+double ts_sec(struct timespec ts);
+struct timespec ts_diff(struct timespec start, struct timespec end);
+double measure_cps(void);
+
+double ts_sec(struct timespec ts)
+{
+  return ((double)(ts.tv_sec)) + ((double)(ts.tv_nsec))/1.0e9;
+}
+
+struct timespec ts_diff(struct timespec start, struct timespec end)
+{
+  struct timespec temp;
+  if ((end.tv_nsec-start.tv_nsec)<0) {
+    temp.tv_sec = end.tv_sec-start.tv_sec-1;
+    temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+  } else {
+    temp.tv_sec = end.tv_sec-start.tv_sec;
+    temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+  }
+  return temp;
+}
+
+double measure_cps()
+{
+  struct timespec cal_start, cal_end;
+  mcps_tctr tsc_start, tsc_end;
+  double total_time;
+  double total_cycles;
+  /* We perform a chaotic iteration and print the result, to defeat
+     compiler optimisation */
+  double chaosC = -1.8464323952913974; double z = 0.0;
+  long int i, ilim, j;
+
+  /* Do it twice and throw away results from the first time; this ensures the
+   * OS and CPU will notice it's busy and set the clock speed. */
+  for(j=0; j<2; j++) {
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cal_start);
+    MCPS_RDTSC(tsc_start);
+    ilim = 100*1000*1000;
+    for (i=0; i<ilim; i++)
+      z = z * z + chaosC;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cal_end);
+    MCPS_RDTSC(tsc_end);
+  }
+
+  total_time = ts_sec(ts_diff(cal_start, cal_end));
+  total_cycles = (double)(tsc_end.int64-tsc_start.int64);
+  CPS = total_cycles / total_time;
+  printf("z == %f, CPS == %g\n", z, CPS);
+
+  return CPS;
+}
+/* ---------------------------------------------------------------------------
+| End of measure_cps code
+--------------------------------------------------------------------------- */
+
+int clock_gettime(clockid_t clk_id, struct timespec *tp);
+vec_ptr new_vec(long int len);
+int get_vec_element(vec_ptr v, long int index, data_t *dest);
+long int get_vec_length(vec_ptr v);
+int set_vec_length(vec_ptr v, long int index);
+int init_vec_zero(vec_ptr v, long int len);
+int init_vec_low(vec_ptr v, long int len);
+int init_vec_mid(vec_ptr v, long int len);
+int init_vec_hi_lo(vec_ptr v, long int len);
+int init_vec_rand(vec_ptr v, long int len);
+int init_vec_linear(vec_ptr v, long int len);
+void branch1(vec_ptr v0, vec_ptr v1, vec_ptr v2);
+void branch2(vec_ptr v0, vec_ptr v1, vec_ptr v2);
+
 /*****************************************************************************/
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   int OPTION;
+  long int elements[ITERS+1];
   struct timespec diff(struct timespec start, struct timespec end);
   struct timespec time1, time2;
   struct timespec time_stamp[OPTIONS][ITERS+1];
-  int clock_gettime(clockid_t clk_id, struct timespec *tp);
-  vec_ptr new_vec(long int len);
-  int get_vec_element(vec_ptr v, long int index, data_t *dest);
-  long int get_vec_length(vec_ptr v);
-  int set_vec_length(vec_ptr v, long int index);
-  int init_vector1(vec_ptr v, long int len);
-  int init_vector2(vec_ptr v, long int len);
-  void branch1(vec_ptr v0, vec_ptr v1, vec_ptr v2);
-  void branch2(vec_ptr v0, vec_ptr v1, vec_ptr v2);
 
   long int i, j, k;
   long long int time_sec, time_ns;
-  long int MAXSIZE = BASE+(ITERS+1)*DELTA;
+  long int MAXSIZE = ITER_VECLEN(ITERS);
 
-  printf("\n Hello World -- psum examples\n");
+  measure_cps();
 
-  // declare vector structure
-  vec_ptr v0 = new_vec(MAXSIZE);
-  vec_ptr v1 = new_vec(MAXSIZE);
-  vec_ptr v2 = new_vec(MAXSIZE);
+  // declare vector structure         // init the vectors
+  vec_ptr v0 = new_vec(MAXSIZE);      init_vec_zero(v0, MAXSIZE);
+  vec_ptr vlow = new_vec(MAXSIZE);    init_vec_low(v0, MAXSIZE);
+  vec_ptr vmid = new_vec(MAXSIZE);    init_vec_mid(vmid, MAXSIZE);
+  vec_ptr vhilo = new_vec(MAXSIZE);   init_vec_hi_lo(vhilo, MAXSIZE);
+  vec_ptr vrand = new_vec(MAXSIZE);   init_vec_rand(vrand, MAXSIZE); 
+  vec_ptr vlin = new_vec(MAXSIZE);    init_vec_linear(vlin, MAXSIZE);
 
-  // init the vectors
-  init_vector1(v0, MAXSIZE);
-  init_vector1(v1, MAXSIZE);
-
-  // execute and time all options 
-  OPTION = 0;
-  for (i = 0; i < ITERS; i++) {
-    set_vec_length(v0,BASE+(i+1)*DELTA);
-    set_vec_length(v1,BASE+(i+1)*DELTA);
-    set_vec_length(v2,BASE+(i+1)*DELTA);
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
-    branch1(v0, v1, v2);
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    time_stamp[OPTION][i] = diff(time1,time2);
-  }
-
-  // init the vectors
-  init_vector2(v0, MAXSIZE);
-  init_vector2(v1, MAXSIZE);
-
-  OPTION++;
-  for (i = 0; i < ITERS; i++) {
-    set_vec_length(v0,BASE+(i+1)*DELTA);
-    set_vec_length(v1,BASE+(i+1)*DELTA);
-    set_vec_length(v2,BASE+(i+1)*DELTA);
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
-    branch1(v0, v1, v2);
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    time_stamp[OPTION][i] = diff(time1,time2);
-  }
-
-  // init the vectors
-  init_vector1(v0, MAXSIZE);
-  init_vector1(v1, MAXSIZE);
+  vec_ptr v_out = new_vec(MAXSIZE);   init_vec_zero(v_out, MAXSIZE);
 
   // execute and time all options 
-  OPTION++;
   for (i = 0; i < ITERS; i++) {
-    set_vec_length(v0,BASE+(i+1)*DELTA);
-    set_vec_length(v1,BASE+(i+1)*DELTA);
-    set_vec_length(v2,BASE+(i+1)*DELTA);
+    set_vec_length(v0,   ITER_VECLEN(i));
+    set_vec_length(vlow, ITER_VECLEN(i));
+    set_vec_length(vmid, ITER_VECLEN(i));
+    set_vec_length(vhilo,ITER_VECLEN(i));
+    set_vec_length(vrand,ITER_VECLEN(i));
+    set_vec_length(vlin, ITER_VECLEN(i));
+    set_vec_length(v_out,ITER_VECLEN(i));
+    elements[i] = ITER_VECLEN(i);
+
+    OPTION = 0;
+
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
-    branch2(v0, v1, v2);
+    branch1(v0, vmid, v_out);
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
     time_stamp[OPTION][i] = diff(time1,time2);
-  }
+    OPTION++;
 
-  // init the vectors
-  init_vector2(v0, MAXSIZE);
-  init_vector2(v1, MAXSIZE);
-
-  OPTION++;
-  for (i = 0; i < ITERS; i++) {
-    set_vec_length(v0,BASE+(i+1)*DELTA);
-    set_vec_length(v1,BASE+(i+1)*DELTA);
-    set_vec_length(v2,BASE+(i+1)*DELTA);
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
-    branch2(v0, v1, v2);
+    branch2(v0, vmid, v_out);
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
     time_stamp[OPTION][i] = diff(time1,time2);
+    OPTION++;
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    branch1(vlow, vmid, v_out);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+    time_stamp[OPTION][i] = diff(time1,time2);
+    OPTION++;
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    branch2(vlow, vmid, v_out);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+    time_stamp[OPTION][i] = diff(time1,time2);
+    OPTION++;
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    branch1(vmid, vlin, v_out);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+    time_stamp[OPTION][i] = diff(time1,time2);
+    OPTION++;
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    branch2(vmid, vlin, v_out);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+    time_stamp[OPTION][i] = diff(time1,time2);
+    OPTION++;
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    branch1(vmid, vhilo, v_out);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+    time_stamp[OPTION][i] = diff(time1,time2);
+    OPTION++;
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    branch2(vmid, vhilo, v_out);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+    time_stamp[OPTION][i] = diff(time1,time2);
+    OPTION++;
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    branch1(vmid, vrand, v_out);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+    time_stamp[OPTION][i] = diff(time1,time2);
+    OPTION++;
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+    branch2(vmid, vrand, v_out);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+    time_stamp[OPTION][i] = diff(time1,time2);
+    OPTION++;
   }
+
+  char filename[255] = {0};
+  FILE *fp;
+
+  sprintf(filename, "%sB%d_D%d_I%d.csv", FILE_PREFIX, BASE, DELTA, ITERS);
+  printf("Current File: %s\n", filename);
+  fp = fopen(filename, "w");
 
   /* output times */
+  fprintf(fp, "Vector Size, ");
+  fprintf(fp, "0-mid ifelse, 0-mid ?:, ");
+  fprintf(fp, "low-mid ifelse, low-mid ?:, ");
+  fprintf(fp, "linear ifelse, linear ?:, ");
+  fprintf(fp, "hilo ifelse, hilo ?:, ");
+  fprintf(fp, "rand ifelse, rand ?:, ");
+  fprintf(fp, "\n");
   for (i = 0; i < ITERS; i++) {
-    printf("\n%d,  ", BASE+(i+1)*DELTA);
-    for (j = 0; j < OPTIONS; j++) {
-      if (j != 0) printf(", ");
-       printf("%ld", (long int)((double)(CPG)*(double)
-		 (GIG * time_stamp[j][i].tv_sec + time_stamp[j][i].tv_nsec)));
+    double cycles;
+    int opt;
+    fprintf(fp, "%ld", ((long int) ITER_VECLEN(i)));
+    for(opt=0; opt<OPTIONS; opt++) {
+      cycles = CPS * ts_sec(time_stamp[opt][i]);
+      fprintf(fp, ", %f", cycles / elements[opt]);
     }
+    fprintf(fp, "\n");
   }
-
-  printf("\n");
+  fclose(fp);
 }/* end main */
 
 /**********************************************/
@@ -178,27 +294,94 @@ int set_vec_length(vec_ptr v, long int index)
   return 1;
 }
 
-/* initialize vector with first pattern */
-int init_vector1(vec_ptr v, long int len)
+/* initialize vector with all zeros */
+int init_vec_zero(vec_ptr v, long int len)
 {
   long int i;
 
   if (len > 0) {
     v->len = len;
-    for (i = 0; i < len; i++) v->data[i] = ;
+    for (i = 0; i < len; i++) {
+      v->data[i] = 0.0;
+    }
     return 1;
   }
   else return 0;
 }
 
-/* initialize vector with another */
-int init_vector2(vec_ptr v, long int len)
+/* initialize vector with all low values */
+int init_vec_low(vec_ptr v, long int len)
 {
   long int i;
 
   if (len > 0) {
     v->len = len;
-    for (i = 0; i < len; i++) v->data[i] = ;
+    for (i = 0; i < len; i++) {
+      v->data[i] = 1.0;
+    }
+    return 1;
+  }
+  else return 0;
+}
+
+/* initialize vector with all-"middle" values */
+int init_vec_mid(vec_ptr v, long int len)
+{
+  long int i;
+
+  if (len > 0) {
+    v->len = len;
+    for (i = 0; i < len; i++) {
+      v->data[i] = 50.0;
+    }
+    return 1;
+  }
+  else return 0;
+}
+
+/* initialize vector with alternating high and low values */
+int init_vec_hi_lo(vec_ptr v, long int len)
+{
+  long int i;
+
+  if (len > 0) {
+    v->len = len;
+    for (i = 0; i < len; i++) {
+      v->data[i] = (i % 2) ? 1.0 : 99.0;
+    }
+    return 1;
+  }
+  else return 0;
+}
+
+/* initialize vector with random values */
+int init_vec_rand(vec_ptr v, long int len)
+{
+  long int i;
+  double ratio = 100.0 / ((double)RAND_MAX);
+
+  if (len > 0) {
+    v->len = len;
+    for (i = 0; i < len; i++) {
+      v->data[i] = ((double)rand()) * ratio;
+    }
+    return 1;
+  }
+  else return 0;
+}
+
+/* initialize vector with steadily increasing values, going from 0 to 100 */
+int init_vec_linear(vec_ptr v, long int len)
+{
+  long int i;
+  double ratio;
+
+  if (len > 0) {
+    v->len = len;
+    ratio = 100.0 / ((double)len);
+    for (i = 0; i < len; i++) {
+      v->data[i] = ((double)i) * ratio;
+    }
     return 1;
   }
   else return 0;
