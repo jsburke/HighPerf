b8a1b174796892e5d0ba8b3cab77d25b2ffb3689
jsburke <jsburke@bu.edu>
2017-02-15 12:32:57 -0500

    set printing, some timing

--- Lab3/code/test_combine8.c
+++ Lab3/code/test_combine8.c
@@ -15,18 +15,21 @@
 #include <math.h>
 
 #define GIG 1000000000
-#define CPG 2.0           // Cycles per GHz -- Adjust to your computer
+#define CPG 2.9           // Cycles per GHz -- Adjust to your computer
+double CPS = 2.9e9;    // Cycles per second -- Will be recomputed at runtime
 
 #define SIZE 10000000
-#define ITERS 30
-#define DELTA 32
-#define BASE 0
+// #define ITERS 30
+// #define DELTA 32
+// #define BASE 0
 
 #define OPTIONS 4       // NEED TO MODIFY
 #define IDENT 1.0
-#define OP *
+#define OP +
 
-typedef double data_t;
+#define FILE_PREFIX ((const unsigned char*) "comb8_")
+
+typedef float data_t;
 
 /* Create abstract data type for vector */
 typedef struct {
@@ -46,47 +49,99 @@ typedef union {
   data_t d[VSIZE];
 } pack_t;
 
+vec_ptr new_vec(long int len);
+int get_vec_element(vec_ptr v, long int index, data_t *dest);
+long int get_vec_length(vec_ptr v);
+int set_vec_length(vec_ptr v, long int index);
+int init_vector(vec_ptr v, long int len);
+
+///////////  Timing Related   /////////////////////////////////////////////////
+
+//rdtsc related
+typedef union {
+  unsigned long long int64;
+  struct {unsigned int lo, hi;} int32;
+} mcps_tctr;
+
+#define MCPS_RDTSC(cpu_c) __asm__ __volatile__ ("rdtsc" : \
+                     "=a" ((cpu_c).int32.lo), "=d"((cpu_c).int32.hi))
+
+int clock_gettime(clockid_t clk_id, struct timespec *tp);
+struct timespec diff(struct timespec start, struct timespec end);
+double ts_sec(struct timespec ts);
+struct timespec ts_diff(struct timespec start, struct timespec end);
+double measure_cps(void);
+
+//////////////  functions for this file ///////////////////////////////////////
+
+void combine4(vec_ptr v, data_t *dest);
+void combine6_5(vec_ptr v, data_t *dest);
+void combine8(vec_ptr v, data_t *dest);
+void combine8_4(vec_ptr v, data_t *dest);
+
 /*****************************************************************************/
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+  int BASE, DELTA, ITERS;
+
+  if(argc != 4)
+  {
+  	printf("num args wrong\n");
+  	return 0;
+  }
+
+  BASE  = strtol(argv[1], NULL, 10);
+  DELTA = strtol(argv[2], NULL, 10);
+  ITERS = strtol(argv[3], NULL, 10);
+
+  if(DELTA == 0)
+  {
+  	printf("DELTA must be greater than zero\n");
+  	return 0;
+  }
+
+  if(ITERS == 0)
+  {
+  	printf("ITERS must be at least one\n");
+  	return 0;
+  }
+
   int OPTION;
 
-  struct timespec diff(struct timespec start, struct timespec end);
   struct timespec time1, time2;
-  struct timespec time_stamp[OPTIONS][ITERS+1];
-  int clock_gettime(clockid_t clk_id, struct timespec *tp);
- 
-  vec_ptr new_vec(long int len);
-  int get_vec_element(vec_ptr v, long int index, data_t *dest);
-  long int get_vec_length(vec_ptr v);
-  int set_vec_length(vec_ptr v, long int index);
-  int init_vector(vec_ptr v, long int len);
-  data_t *data_holder;
+  struct timespec time_stamp[OPTIONS][ITERS+1]; 
 
-  void combine4(vec_ptr v, data_t *dest);
-  void combine6_5(vec_ptr v, data_t *dest);
-  void combine8(vec_ptr v, data_t *dest);
-  void combine8_4(vec_ptr v, data_t *dest);
+  data_t *data_holder;
 
   long int i, j, k;
   long long int time_sec, time_ns;
   long int MAXSIZE = BASE+(ITERS+1)*DELTA;
 
-  printf("\n Hello World -- vector examples\n");
+  char filename[255] = {0};
+  FILE *fp;
+
+  sprintf(filename, "%sB%d_D%d_I%d.csv", FILE_PREFIX, BASE, DELTA, ITERS);
+  printf("Current File: %s\n",filename);
+
+  measure_cps();
 
   // declare and initialize the vector structure
   vec_ptr v0 = new_vec(MAXSIZE);
   data_holder = (data_t *) malloc(sizeof(data_t));
   init_vector(v0, MAXSIZE);
 
-  // execute and time ... options from B&O 
+  ////////////////////////////////////////////////
+  //
+  //  Begin combine calls
+  //
+  ////////////////////////////////////////////////
   OPTION = 0;
   for (i = 0; i < ITERS; i++) {
     set_vec_length(v0,BASE+(i+1)*DELTA);
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
     combine4(v0, data_holder);
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    time_stamp[OPTION][i] = diff(time1,time2);
+    time_stamp[OPTION][i] = ts_diff(time1,time2);
   }
 
   OPTION++;
@@ -95,7 +150,7 @@ main(int argc, char *argv[])
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
     combine6_5(v0, data_holder);
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    time_stamp[OPTION][i] = diff(time1,time2);
+    time_stamp[OPTION][i] = ts_diff(time1,time2);
   }
 
   OPTION++;
@@ -104,7 +159,7 @@ main(int argc, char *argv[])
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
     combine8(v0, data_holder);
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    time_stamp[OPTION][i] = diff(time1,time2);
+    time_stamp[OPTION][i] = ts_diff(time1,time2);
   }
 
   OPTION++;
@@ -113,23 +168,124 @@ main(int argc, char *argv[])
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
     combine8_4(v0, data_holder);
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-    time_stamp[OPTION][i] = diff(time1,time2);
+    time_stamp[OPTION][i] = ts_diff(time1,time2);
   }
 
-  /* output times */
-  printf("\nsize, c4, c6_5,  c8,  c8_4");
+  ///////////////////////////////////////////////////////
+  //
+  //  Write to file
+  //
+  ///////////////////////////////////////////////////////
+
+  fp = fopen(filename,"w");
+  fprintf(fp,"\nsize, c4, c6_5,  c8,  c8_4\n");
+
+  int elements;
+
   for (i = 0; i < ITERS; i++) {
-    printf("\n%d, ", BASE+(i+1)*DELTA);
+  	elements = BASE+(i+1)*DELTA;
+    fprintf(fp, "%d, ", elements);
     for (j = 0; j < OPTIONS; j++) {
-      if (j != 0) printf(", ");
-      printf("%ld", (long int)((double)(CPG)*(double)
-		 (GIG * time_stamp[j][i].tv_sec + time_stamp[j][i].tv_nsec)));
+      if (j != 0) fprintf(fp, ", ");
+      fprintf(fp, "%lf", ((double)(CPG)*(double)(GIG * time_stamp[j][i].tv_sec + time_stamp[j][i].tv_nsec)/elements));
     }
+    fprintf(fp, "\n");
   }
 
-  printf("\n");
+  fclose(fp);
+  return 0;
   
 }/* end main */
+
+//////////////////////////////  TIming related  ////////////////////////////////
+
+double ts_sec(struct timespec ts)
+{
+  return ((double)(ts.tv_sec)) + ((double)(ts.tv_nsec))/1.0e9;
+}
+
+/* ---------------------------------------------------------------------------
+| Make the CPU busy, and measure CPS (cycles per second).
+|
+| Explanation:
+| If tests are very fast, they can run so quickly that the SpeedStep control
+| (in kernel and/or on-chip) doesn't notice in time, and the first few tests
+| might finish while the CPU is still in its sleep state (about 800 MHz,
+| judging from my measurements)
+|   A simple way to get around this is to run some kind of busy-loop that
+| forces the OS and/or CPU to notice it needs to go to full clock speed.
+| We print out the results of the computation so the loop won't get optimised
+| away.
+|
+| Copy this code into other programs as desired. It provides three entry
+| points:
+|
+| double ts_sec(ts): converts a timespec into seconds
+| timespec ts_diff(ts1, ts2): computes interval between two timespecs
+| measure_cps(): Does the busy loop and prints out measured CPS (cycles/sec)
+--------------------------------------------------------------------------- */
+
+struct timespec ts_diff(struct timespec start, struct timespec end)
+{
+  struct timespec temp;
+  if ((end.tv_nsec-start.tv_nsec)<0) {
+    temp.tv_sec = end.tv_sec-start.tv_sec-1;
+    temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+  } else {
+    temp.tv_sec = end.tv_sec-start.tv_sec;
+    temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+  }
+  return temp;
+}
+
+double measure_cps()
+{
+  struct timespec cal_start, cal_end;
+  mcps_tctr tsc_start, tsc_end;
+  double total_time;
+  double total_cycles;
+  /* We perform a chaotic iteration and print the result, to defeat
+     compiler optimisation */
+  double chaosC = -1.8464323952913974; double z = 0.0;
+  long int i, ilim, j;
+
+  /* Do it twice and throw away results from the first time; this ensures the
+   * OS and CPU will notice it's busy and set the clock speed. */
+  for(j=0; j<2; j++) {
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cal_start);
+    MCPS_RDTSC(tsc_start);
+    ilim = 50*1000*1000;
+    for (i=0; i<ilim; i++)
+      z = z * z + chaosC;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cal_end);
+    MCPS_RDTSC(tsc_end);
+  }
+
+  total_time = ts_sec(ts_diff(cal_start, cal_end));
+  total_cycles = (double)(tsc_end.int64-tsc_start.int64);
+  CPS = total_cycles / total_time;
+  printf("z == %f, CPS == %g\n", z, CPS);
+
+  return CPS;
+}
+/* ---------------------------------------------------------------------------
+| End of measure_cps code
+--------------------------------------------------------------------------- */
+
+struct timespec diff(struct timespec start, struct timespec end)
+{
+  struct timespec temp;
+  if ((end.tv_nsec-start.tv_nsec)<0) {
+    temp.tv_sec = end.tv_sec-start.tv_sec-1;
+    temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+  } else {
+    temp.tv_sec = end.tv_sec-start.tv_sec;
+    temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+  }
+  return temp;
+}
+
+//////////////////////////////  End Timing Related //////////////////////////////
 
 /**********************************************/
 /* Create vector of specified length */
@@ -197,19 +353,6 @@ data_t *get_vec_start(vec_ptr v)
   return v->data;
 }
 
-/*************************************************/
-struct timespec diff(struct timespec start, struct timespec end)
-{
-  struct timespec temp;
-  if ((end.tv_nsec-start.tv_nsec)<0) {
-    temp.tv_sec = end.tv_sec-start.tv_sec-1;
-    temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
-  } else {
-    temp.tv_sec = end.tv_sec-start.tv_sec;
-    temp.tv_nsec = end.tv_nsec-start.tv_nsec;
-  }
-  return temp;
-}
 
 /*************************************************/
 /* Combine4:  Accumulate result in local variable
