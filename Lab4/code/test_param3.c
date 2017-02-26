3f9392b4ffe43bcb84cc60c96f27c76a2d9dc742
jsburke <jsburke@bu.edu>
2017-02-26 10:56:02 -0500

    task 11 complete

--- Lab4/code/test_param3.c
+++ Lab4/code/test_param3.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define NUM_THREADS 5
+#define NUM_THREADS 6
 
 struct thread_data{
   int thread_id;
@@ -43,13 +43,15 @@ int main(int argc, char *argv[])
 				 "Second Message",
 				 "Third Message",
 				 "Fourth Message",
-				 "Fifth Message"};
+				 "Fifth Message",
+         "Sixth Message"};
 
   printf("\n Hello World!  It's me, MAIN!\n");
 
   for (t = 0; t < NUM_THREADS; t++) {
     thread_data_array[t].thread_id = t;
-    thread_data_array[t].sum = t+28;
+    if(t == (NUM_THREADS-1)) thread_data_array[t].sum = 1000;
+    else thread_data_array[t].sum = t+28;
     thread_data_array[t].message = Messages[t];
     printf("In main:  creating thread %ld\n", t);
     rc = pthread_create(&threads[t], NULL, PrintHello,
