6d9e599bbcd5c7b91ab1dfc06b7728c0b53d6180
jsburke <jsburke@bu.edu>
2017-03-14 16:29:28 -0400

    add script for part 3

---
+++ Lab5/scripts/ptBuild.pl
@@ -0,0 +1,46 @@
+: # -*- perl -*-
+  eval 'exec perl -S  $0 ${1+"$@"}' 
+    if 0;  # if running under some shell
+
+$myname = "ptBuild";
+
+$cwd = `pwd`; chomp $cwd;
+if (!($cwd =~ m|/code$|)) {
+  die "$myname: Curdir should be the 'code' directory.\n";
+}
+
+$bin = "./PT_go";
+if (-e $bin) {
+  unlink($bin);
+}
+
+$src = "test_pt.c";
+if(!(-f $src)) {
+  die "$myname: Source file $src not found.\n";
+}
+
+print "compiling $src to $bin\n";
+system("gcc -pthread -lrt $src -o $bin -lm");
+if (!(-x $bin)) {
+  die "$myname: No binary $bin, compile error?\n";
+}
+
+system("$bin 10 5 38");  # up to length of 200
+print "\n";
+
+
+$ddir = "part3_data";
+if(!(-d $ddir)) {
+  print "making directory for data output files\n";
+  system("mkdir $ddir");
+}
+$ncsv = (`ls *.csv | wc -l`) + 0;
+if ($ncsv > 0) {
+  system("mv *.csv $ddir");
+} else {
+  die "$myname: No .csv output files were produced.\n";
+}
+
+system("rm $bin");
+
+print "test_pt tests complete\n";
